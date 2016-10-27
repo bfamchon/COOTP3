a2c609e7ae4c8d6544f43fd5a88d220020151b95
UniP62 <baptiste.famchon@sfr.fr>
2016-10-27 21:09:11 +0200

    Ajout umls

--- src/com/cf/constante/Constante.java
+++ src/com/cf/constante/Constante.java
@@ -8,7 +8,7 @@ public class Constante {
 	public static final String CONNEXION_CONFIG = "jdbc:mysql://webtp.fil.univ-lille1.fr/famchon?" +
 					                               "user=famchon&password=BLABLA";
 
-	public static final String CONNEXION_CONFIG_BAPTISTE = "jdbc:mysql://localhost/bddtp3?" + "user=BLABLA&password=BLABLA";
+	public static final String CONNEXION_CONFIG_BAPTISTE = "jdbc:mysql://localhost/bddtp3?" + "user=&password=";
 	public static final String CONNEXION_CONFIG_LAURENT = "jdbc:mysql://localhost/tp3java?" + "user=root";
 
 }
